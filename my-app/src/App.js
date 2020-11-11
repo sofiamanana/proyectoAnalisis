9a1ac88ff3268862f06047120020a8651abdbfd0
Sofia <sofia.manana@sansano.usm.cl>
2020-11-11 00:28:10 +0000

    app react

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+
+function App() {
+  return (
+    <div>
+      <p>Holi</p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
